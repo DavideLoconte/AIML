697ea801287dc18fe855ebd73c4062b602719b98
DavideLoconte <davide.loconte@hotmail.it>
2019-10-07 21:06:42 +0200

    Change directory structure and Tree Search optimizations

---
+++ examples/puzzle.py
@@ -0,0 +1,101 @@
+from graph import Graph
+
+
+class Puzzle(Graph):
+    UP = (-1, 0)
+    DOWN = (1, 0)
+    LEFT = (0, -1)
+    RIGHT = (0, 1)
+
+    def __init__(self, table):
+        super().__init__()
+        solution1 = (
+            (1, 2, 3),
+            (4, 5, 6),
+            (7, 8, 0)
+        )
+        solution2 = (
+            (0, 1, 2),
+            (3, 4, 5),
+            (6, 7, 8)
+        )
+        self.solutions.append(solution1)
+        self.solutions.append(solution2)
+        self.add_node(solution1)
+        self.add_node(solution2)
+        self.add_node(table)
+        self.set_starting_node(table)
+        self.visited = set()
+
+    @staticmethod
+    def get_hole_position(table):
+        for row in table:
+            for number in row:
+                if number == 0:
+                    return tuple([table.index(row), row.index(number)])
+        raise Exception("Cannot find hole, table = {}".format(str(table)))
+
+    @staticmethod
+    def __tuple_sum(a, b):
+        if len(a) != 2 or len(b) != 2:
+            raise Exception("Wrong tuple format")
+        return tuple((
+            a[0] + b[0],
+            a[1] + b[1]
+        ))
+
+    @staticmethod
+    def __swap(table: tuple, a: tuple, b: tuple):
+        lst = list()
+        for row in table:
+            lst.append(list(row))
+        lst[a[0]][a[1]],  lst[b[0]][b[1]] = lst[b[0]][b[1]],  lst[a[0]][a[1]]
+        row0 = tuple(lst[0])
+        row1 = tuple(lst[1])
+        row2 = tuple(lst[2])
+        return tuple((row0, row1, row2))
+
+    @staticmethod
+    def move(table, direction):
+        current_position = Puzzle.get_hole_position(table)
+        new_position = Puzzle.__tuple_sum(current_position, direction)
+        if new_position[0] in range(3) and new_position[1] in range(3):
+            return Puzzle.__swap(table, current_position, new_position)
+        else:
+            return table
+
+    @staticmethod
+    def is_table_valid(table):
+        if len(table) != 3:
+            print("Warning, invalid col size")
+            return False
+        for row in table:
+            if len(row) != 3:
+                print("Warning, invalid row size")
+                return False
+        return True
+
+    # Override
+    def successor(self, node):
+        if node is None:
+            node = self.starting_node
+
+        if node in self.visited:
+            return []
+        moves = [
+            self.move(node, Puzzle.UP),
+            self.move(node, Puzzle.DOWN),
+            self.move(node, Puzzle.LEFT),
+            self.move(node, Puzzle.RIGHT)
+        ]
+        while node in moves:
+            moves.remove(node)
+
+        result = []
+        for successor in moves:
+            path = tuple((node, successor))
+            self.add_path(path, 1, True)
+            result.append([successor, 1])
+        self.visited.add(node)
+        return result
+
---
+++ lib/tree.py
@@ -0,0 +1,101 @@
+from graph import Graph
+import heapq
+
+class TreeNode:
+    def __init__(self, node, total_cost, depth, parent):
+        self.node = node
+        self.cost = total_cost
+        self.depth = depth
+        self.parent = parent
+
+    def __lt__(self, other):
+        return self.cost < other.cost
+
+
+def get_complete_path(last_node: TreeNode):
+    complete_path = list()
+    cost = last_node.cost
+    depth = last_node.depth
+    while last_node.parent is not None:
+        complete_path.append(last_node.node)
+        last_node = last_node.parent
+    complete_path.append(last_node.node)
+    complete_path.reverse()
+    result = {
+        "path": complete_path,
+        "cost": cost,
+        "depth": depth
+    }
+    return result
+
+
+class TreeSearch:
+    def __init__(self, graph: Graph):
+        self.graph = graph
+        self.root = TreeNode(
+            node=graph.get_starting_node(),
+            total_cost=0,
+            depth=0,
+            parent=None)
+        self.fringe = [self.root]
+        self.next_node = self.root
+        self.iteration = 0
+        self.visited = set()
+
+    def expand(self, node):
+        successor_function = self.graph.successor(node.node)
+        new_nodes_list = []
+        for successor in successor_function:
+            new_node = TreeNode(
+                node=successor[0],
+                total_cost=node.cost + successor[1],
+                depth=node.depth + 1,
+                parent=node
+            )
+            new_nodes_list.append(new_node)
+        return new_nodes_list
+
+    def iterative_search(self, node_selector, log=False):
+        while True:
+            self.iteration += 1
+            if log:
+                print("Iteration: {}".format(self.iteration))
+                print("Depth: {}\t Fringe_len: {}".format(self.next_node.depth, len(self.fringe)))
+                print(": {}".format(str(self.fringe)))
+
+            if len(self.fringe) != 0:
+                self.next_node = node_selector(self.fringe)
+                if self.graph.is_solution(self.next_node.node):
+                    return get_complete_path(self.next_node)
+                else:
+                    if self.next_node in self.visited:
+                        continue
+
+                    new_nodes = self.expand(self.next_node)
+                    for new_node in new_nodes:
+                        self.fringe.append(new_node)
+                    self.visited.add(self.next_node)
+            else:
+                return False
+
+    def search(self, node_selector=None, log=False):
+        if node_selector is None:
+            if log:
+                print("No node selection algorithm specified, falling back to breadth_first")
+            node_selector = fifo
+        if log:
+            print("Starting iterative search")
+        return self.iterative_search(node_selector, log)
+
+
+def fifo(fringe: list):
+    return fringe.pop(0)
+
+
+def lifo(fringe: list):
+    return fringe.pop(-1)
+
+
+def least_cost(fringe):
+    fringe.sort(key=lambda x: x.cost)
+    return fringe.pop(0)
